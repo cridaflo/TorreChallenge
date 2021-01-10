a0b0341957f9b10efd8d38bbcf77c5b2ba491e36
cridaflo <cd.florez10@uniandes.edu.co>
2021-01-10 17:44:51 -0500

    basic structure of the design

--- frontend/src/app/components/professional-dynamics-selector/professional-dynamics-selector.component.ts
+++ frontend/src/app/components/professional-dynamics-selector/professional-dynamics-selector.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { CulturalProfileService } from 'src/app/services/cultural-profile.service';
+import { ProfessionalDynamicsService } from 'src/app/services/professional-dynamics.service';
 
 @Component({
   selector: 'app-professional-dynamics-selector',
@@ -6,10 +9,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./professional-dynamics-selector.component.scss']
 })
 export class ProfessionalDynamicsSelectorComponent implements OnInit {
+    
+    professionalDynamicsList: any;
+    selectedDynamics: any = [];
 
-  constructor() { }
+    constructor(
+        private profDynamicService: ProfessionalDynamicsService,
+        private culProfileService: CulturalProfileService,
+        private router: Router
+    ) {
 
-  ngOnInit(): void {
-  }
+    }
+    ngOnInit(): void {
+        this.getProfessionalDynamics();
+    }
 
+    getProfessionalDynamics() {
+        this.profDynamicService.getProfessionalDynamics()
+        .subscribe( data => {
+            this.professionalDynamicsList = data;
+        })
+    }
+
+    select(item){
+        const index = this.selectedDynamics.indexOf(item);
+        if(index<0 && this.selectedDynamics.length <5){
+            this.selectedDynamics.push(item);
+        } else if(index >= 0) {
+            this.selectedDynamics.splice(index, 1)
+        }
+    }
+
+    generateCulturalProfile() {
+        this.culProfileService.createCulturalProfile(this.selectedDynamics)
+        .subscribe(data => {
+            sessionStorage.setItem('culturalProfile', JSON.stringify(data));
+            this.router.navigate(['/cultural-profile'])
+        })
+    }
 }
