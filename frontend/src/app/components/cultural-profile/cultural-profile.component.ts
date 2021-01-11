ee30a2a2f13da343a37a8896fc92d627fe95c255
cridaflo <cd.florez10@uniandes.edu.co>
2021-01-10 19:10:07 -0500

    basic structure of the component

--- frontend/src/app/components/cultural-profile/cultural-profile.component.ts
+++ frontend/src/app/components/cultural-profile/cultural-profile.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { OnInit, Component, ViewChild} from "@angular/core";
+import ApexCharts from 'apexcharts';
 
 @Component({
   selector: 'app-cultural-profile',
@@ -7,9 +8,61 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CulturalProfileComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  orderedCulturalProfile = [];
+  constructor() {
   }
+
+  ngOnInit() {
+    const culturalProfile = JSON.parse(sessionStorage.getItem('culturalProfile'));
+    console.log(culturalProfile)
+    const data = culturalProfile.map(x => x.average_correlation);
+    const max = data.reduce((a,b)=>Math.max(a,b), -Infinity);
+    const options = {
+      series: [{
+      name: 'Series 1',
+      data: data
+    }],
+      chart: {
+      height: 500,
+      type: 'radar',
+      dropShadow: {
+        enabled: true,
+        blur: 1,
+        left: 1,
+        top: 1
+      }
+    },
+    yaxis: {
+      decimalsInFloat: 0,
+      min: 0, 
+      max: Math.floor((max/5)+1)*5,
+      labels: {
+        formatter: (value) => { return value },
+      }
+    },
+    stroke: {
+      width: 2
+    },
+    fill: {
+      opacity: 0.1
+    },
+    markers: {
+      size: 0
+    },
+    xaxis: {
+      categories: culturalProfile.map(x => x.cultural_dynamic_name)
+    }
+    };
+
+    const chart = new ApexCharts(
+        document.querySelector('#apex-basic-area-chart'),
+        options
+    );
+
+    chart.render();
+    this.orderedCulturalProfile = culturalProfile.sort( (e1, e2) => e2.average_correlation-e1.average_correlation)
+}
+
+
 
 }
