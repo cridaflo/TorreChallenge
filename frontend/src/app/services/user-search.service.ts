adb8c431ca42ce01e2231130991a77c9c2cb2417
cridaflo <cd.florez10@uniandes.edu.co>
2021-01-10 19:28:51 -0500

    implementention of the service to retirve user from torre api

---
+++ frontend/src/app/services/user-search.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserSearchService {
+
+  private APIurl = ' https://search.torre.co/people/_search/?[offset=$offset&size=$size&aggregate=$aggregate]'
+  constructor(
+    private http: HttpClient
+  ) { }
+
+  getUsersByName(name :string, offset: number, size: number) {
+    const url = this.APIurl.replace('$offset', offset.toString()).replace('$size', size.toString());
+    return this.http.post(url, {
+      name: {term: name}
+    });
+
+  }
+}
