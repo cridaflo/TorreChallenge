e2febb9d8aca3479caee84c7dc88192902359e9a
cridaflo <cd.florez10@uniandes.edu.co>
2021-01-12 10:18:18 -0500

    self signed certificate management

--- backend/run.py
+++ backend/run.py
@@ -2,5 +2,4 @@ from app import app
 from app import retrieve_initial_data
 
 retrieve_initial_data()
-context = ('cert.pem', 'key.pem')
-app.run(port=3000, debug=True, ssl_context=context)
+app.run(port=3000, debug=True, ssl_context='adhoc')
