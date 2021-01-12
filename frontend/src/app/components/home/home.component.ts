57df8a891588d09bb014993f470d917da47df366
cridaflo <cd.florez10@uniandes.edu.co>
2021-01-11 23:29:23 -0500

    dashboard implementation and adition of back buttons

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  montecarloImage = '../../../assets/montecarloAlt.PNG';
+  comparisonExampleImage = '../../../assets/graphExample.PNG'
+
   constructor() { }
 
   ngOnInit(): void {
